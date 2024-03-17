255149d2fd108ef9c30877a3103999da9308fc0b
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-03-17 08:05:03 +0000

    dataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"Use strict"; // treat all JS codes as newer version
+
+//alert("Hello"); //we are using node js , not browser
+
+let name = "Devanshu"
+let age = 22
+
+//number => 2 to the power 53
+//bigint
+//string
+//boolean
+//null  = is a standalone value,it is a object
+//undefined = its is a type not an object
+//symbol => To find the uniqueness
+//
+
+
+
+//object
+console.log( typeof true)
