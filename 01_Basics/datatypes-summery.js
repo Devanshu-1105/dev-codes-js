474cd27e95c3a872bfbc8e9dae90651af645b161
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-03-20 14:38:52 +0000

    modified

--- 01_Basics/datatypes-summery.js
+++ 01_Basics/datatypes-summery.js
@@ -17,21 +17,35 @@
 //Objects
 // Functions
 
-//Array
- let Heros = ["Batman","Superman","Hanuman" ,"Shaktiman"];
+// //Array
+//  let Heros = ["Batman","Superman","Hanuman" ,"Shaktiman"];
   
- console.log(Heros);
+//  console.log(Heros);
 
- //Objects
+//  //Objects
 
-  let MyObj= {
-    Name:"Devanshu Garad",
-    Age: 23,  
- };
- console.log(MyObj);
+//   let MyObj= {
+//     Name:"Devanshu Garad",
+//     Age: 23,  
+//  };
+//  console.log(MyObj);
 
- //Functions
+//  //Functions
 
- let myFunction = function() {
-    console.log("Hello world");
- }
+//  let myFunction = function() {
+//     console.log("Hello world");
+//  }
+//Return type of variables in JavaScript
+// 1) Primitive Datatypes
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
