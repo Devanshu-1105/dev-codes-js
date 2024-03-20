2c4e6746e4a391b4c26042a3253ba2f113a3d870
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-03-20 14:34:11 +0000

    Summery

---
+++ 01_Basics/datatypes-summery.js
@@ -0,0 +1,37 @@
+//#primitive DataTypes
+
+
+// There are 7 dataTypes
+
+// String 
+// Number 
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+//Reference Type (Non-primitive)
+
+//Arrays
+//Objects
+// Functions
+
+//Array
+ let Heros = ["Batman","Superman","Hanuman" ,"Shaktiman"];
+  
+ console.log(Heros);
+
+ //Objects
+
+  let MyObj= {
+    Name:"Devanshu Garad",
+    Age: 23,  
+ };
+ console.log(MyObj);
+
+ //Functions
+
+ let myFunction = function() {
+    console.log("Hello world");
+ }
