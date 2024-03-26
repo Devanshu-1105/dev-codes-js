e0f64bf1f9316db199e2777c22d2824d2f097019
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-03-26 17:21:12 +0000

    num_math

--- 01_Basics/05_num_math.js
+++ 01_Basics/05_num_math.js
@@ -1,2 +1,20 @@
 
- v 
+const otherNumber = 123.90099;
+//console.log(otherNumber.toPrecision(4));  
+
+const hundreds = 1000000000;
+ //console.log(hundreds.toLocaleString('en-IN'));    //convert the currency value to Indain standards.
+
+
+ //+++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  console.log(Math);
+
+//  console.log(Math.abs(-4));   //converts negative value to positive;
+//  console.log(Math.round(4.89)); //rounds off the value: floor and ceil
+
+//random function in math\
+
+const min = 10;
+const max =20 ;
+console.log(Math.floor(Math.random()  * (max - min + 1)) +max);    //Gives you value between 0-1 
