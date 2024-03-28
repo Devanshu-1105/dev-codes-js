064df9512920574a39874b814fc5b48783d46724
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-03-28 17:37:29 +0000

    date-time

---
+++ 01_Basics/06_Date_and_time.js
@@ -0,0 +1,25 @@
+// Date and Time//
+
+// let myDate = new Date();
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+
+//let myCreatedDate = new Date ("02-14-2024")     //MM-DD-YYYY format
+//console.log(myCreatedDate.toDateString())
+
+
+// let timeStamp = Date.now();
+// //console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));  //this wil give you milisec values
+
+// let Dates = new Date();
+// // console.log( Dates.getDate()); //gets you the date 
+// // console.log( Dates.getMonth()+ 1); 
+
+// Dates.toLocaleString ("Default" , {        // can help you create an obj and make customise dates
+//     weekday: "long",
+//     timeZone: "IST",
+// })
+
+// console.log(Dates);
