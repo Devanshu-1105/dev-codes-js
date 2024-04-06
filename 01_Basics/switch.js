51325ed48668aa66ff312cf254a22a69d68e0e6d
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-06 06:34:10 +0000

    Switch statement

---
+++ 01_Basics/switch.js
@@ -0,0 +1,24 @@
+const month = "April"
+
+
+switch (month) {
+    case "Jan":
+        console.log("january");
+        
+        break;
+        case "feb":
+            console.log("feb");
+            
+            break;        
+            case "march":
+        console.log("march");
+        
+        break;
+        case "April":
+        console.log("April");
+        
+        break;
+    default:
+        console.log("default");
+        break;
+}
