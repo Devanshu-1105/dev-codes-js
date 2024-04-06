65b2a210c9e3b94643c3ffa61423360f7800656a
Devanshu-1105 <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-06 07:57:59 +0000

    truthy falsy

---
+++ 01_Basics/truthy_falsy.js
@@ -0,0 +1,36 @@
+//falsy values
+
+// false , 0 ,-0, BigInt 0n, "" , null, undefined,  NaN
+
+//truthy values
+
+// "0" , 'false' , " ", [], {}, function(){}
+
+// const emptyObj = {                                                         
+
+// }
+
+// if(Object.keys(emptyObj).length===0) {
+//     console.log("object empty");
+// }
+
+
+//  // we can similarly detect if the arry is empty or not 
+// const emptyArr = [];
+
+// if(emptyArr.length===0) {
+//     console.log("Array is empty");
+// }
+
+//Nullish  coalescing operator is a fallback operator  is a safety for null and undefined value
+
+//  let val1;
+// //val1 = 5 ?? 10;
+// val1 = null ?? 10
+// console.log(val1);  
+
+
+
+//Terniary operator
+ const iceTeaPrice = 110;
+ iceTeaPrice <= 100 ? console.log("less than 100"): console.log("more than 100")
