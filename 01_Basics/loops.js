0adcef03cea60dcb60352a3e7dfe3cc16f73db99
Devanshu janardan garad <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-08 08:33:27 +0000

    loops

--- 01_Basics/loops.js
+++ 01_Basics/loops.js
@@ -1,7 +1,17 @@
 //loops/iterations
 
 //for
-for (let index = 0; index < 250; index++) {
-   // const element = index;
-    console.log(index);
+// for (let i = 0; i <=10; i++) {
+//    const element = i;
+//    if (i===5) {
+//     console.log("5 is the best number");
+    
+//    }
+//     console.log(i);
+// }
+
+
+for (let i = 0; i <=10; i++) {
+    const element = array[i];
+    
 }
