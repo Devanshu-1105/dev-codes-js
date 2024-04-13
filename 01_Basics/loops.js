80bc4dfd72c5153f090a1953304436e5aee318ec
Devanshu janardan garad <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-13 08:22:19 +0000

    loops

--- 01_Basics/loops.js
+++ 01_Basics/loops.js
@@ -36,8 +36,28 @@
  // do while loop
  // in do while loop the work is done before the condition is checked
 
- let score = 0
- do {
-    console.log(`The score is ${score}`)
-     score++
- } while (score<=10);
+//  let score = 0
+//  do {
+//     console.log(`The score is ${score}`)
+//      score++
+//  } while (score<=10);
+
+
+//for of 
+
+let array = [1,2,3,4,5,6]
+for (const iterator of array) {
+    //console.log(`the value is ${iterator}`);
+    
+}
+const map = new Map ()
+map.set('IN' ,"India")
+map.set('USA' ,"United States of America")
+map.set('Fr' ,"France")
+
+
+//console.log(map);
+for (const [key, value] of map) {        //aray destructuring
+    console.log(key,":" ,value);
+    
+}
