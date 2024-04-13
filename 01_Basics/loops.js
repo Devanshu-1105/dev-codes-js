d2b34f2345e2463f9e3608efc778aed920ab3315
Devanshu janardan garad <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-13 06:28:31 +0000

    loops

--- 01_Basics/loops.js
+++ 01_Basics/loops.js
@@ -11,7 +11,33 @@
 // }
 
 
-for (let i = 0; i <=10; i++) {
-    const element = array[i];
+// for (let i = 0; i <=10; i++) {
+//     const element = array[i];
     
-}
+// }
+ // while loop
+
+//  let index =0
+//  while (index<=10) {
+//     console.log(`value of the index is ${index}`);
+//       index = index+1
+    
+//  }
+
+//  let myArray = ["flash","batman","superman"]
+//  let arr = 0
+//  while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++
+    
+//  }
+
+
+ // do while loop
+ // in do while loop the work is done before the condition is checked
+
+ let score = 0
+ do {
+    console.log(`The score is ${score}`)
+     score++
+ } while (score<=10);
