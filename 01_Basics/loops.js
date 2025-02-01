15d38cab5b1edb3689570e5efcdff2aff1a5997b
Devanshu janardan garad <158127154+Devanshu-1105@users.noreply.github.com>
2025-02-01 17:35:17 +0530

    Update loops.js

--- 01_Basics/loops.js
+++ 01_Basics/loops.js
@@ -9,7 +9,13 @@
 //    }
 //     console.log(i);
 // }
-
+//nested loops
+for(let i=0; i<=10; i++) {
+    console.log(`Outer Loop value:`)
+    for (let j =0; j<=10; j++){
+        console.log(`inner Loop value:`)
+    }
+}
 
 // for (let i = 0; i <=10; i++) {
 //     const element = array[i];
